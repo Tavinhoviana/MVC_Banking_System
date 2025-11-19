f91f1a7a65c4b89dafb7695006793fe2253f0e4f
Otávio Viana Marques dos Santos <otavio_vms@hotmail.com>
2025-11-19 12:36:50 -0300

    config: implementing entities in models for sqlite

---
+++ src/sqlite/entities/pessoa_fisica.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, String, BIGINT, REAL
+from src.sqlite.settings.base import Base
+
+class PessoaFisicaTable(Base):
+    __tablename__ = "pessoa_fisica"
+
+    id = Column(BIGINT, primary_key=True)
+    renda_mensal = Column(REAL, nullable=False)
+    idade = Column(BIGINT, nullable=False)
+    nome_completo = Column(String, nullable=True)
+    celular = Column(String, nullable=False)
+    email = Column(String, nullable=True)
+    categoria = Column(String, nullable=True)
+    saldo = Column(REAL, nullable=False)
+
+    def __repr__(self):
+        return (
+        f"Pessoa juridica [\n"
+        f"  nome_completo={self.nome_completo},\n"
+        f"  idade={self.idade},\n"
+        f"  celular={self.celular},\n"
+        f"  email={self.email},\n"
+        f"  categoria={self.categoria}\n"
+        f"]"
+    )
