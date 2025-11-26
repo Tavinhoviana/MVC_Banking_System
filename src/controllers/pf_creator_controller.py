c432ca2decab9713b5cdc3eadceb315076a45816
Otávio Viana Marques dos Santos <otavio_vms@hotmail.com>
2025-11-26 15:14:36 -0300

    feat: implementing pf_creator_controller with tests

--- src/controllers/pf_creator_controller.py
+++ src/controllers/pf_creator_controller.py
@@ -1,5 +1,39 @@
+from typing import Dict
+import re
 from src.models.sqlite.interfaces.pf_repository_interfaces import PFRepositoryInteface
 
 class PFCreatorController:
     def __init__(self, pf_repository: PFRepositoryInteface) -> None:
         self.__pf_repository = pf_repository
+
+    def create(self, person_info: Dict) -> Dict:
+        nome_completo = person_info["nome_completo"]
+        renda_mensal = person_info["renda_mensal"]
+        idade = person_info["idade"]
+        celular = person_info["celular"]
+        email = person_info["email"]
+        categoria = person_info["categoria"]
+        saldo = person_info["saldo"]
+
+        self.__validate_nome_completo(nome_completo)
+        self.__insert_person_in_db(nome_completo, renda_mensal, idade, celular, email, categoria, saldo)
+        formated_response = self.__format_response(person_info)
+        return formated_response
+
+    def __validate_nome_completo(self: str, nome_completo: str) -> None:
+        non_valid_characters = re.compile(r'[^a-zA-Z]')
+
+        if non_valid_characters.search(nome_completo):
+            raise Exception ("Invalid name")
+    
+    def __insert_person_in_db(self: str, nome_completo: str, renda_mensal: int, idade: int, celular: int, email: str, categoria: str, saldo: int) -> None:
+        self.__pf_repository.create_pf(nome_completo, renda_mensal, idade, celular, email, categoria, saldo)
+
+    def __format_response(self, person_info: Dict) -> Dict:
+        return {
+            "data": {
+                "type": "Person",
+                "count": 1,
+                "attributes": person_info
+            }
+        }
---
+++ src/controllers/pf_creator_controller_test.py
@@ -0,0 +1,39 @@
+import pytest
+from .pf_creator_controller import PFCreatorController
+
+class MockPFRepository:
+    def create_pf(self: str, nome_completo: str, renda_mensal: int, idade: int, celular: int, email: str, categoria: str, saldo: int):
+        pass
+
+def test_create():
+    person_infos = {
+        "nome_completo": "DjalmaDaMalasia",
+        "renda_mensal": 1000,
+        "idade": 18,
+        "celular": 11963683670,
+        "email": "djalma_malasiano@gmail.com",
+        "categoria": "ferro",
+        "saldo": 5000
+    }
+
+    controller = PFCreatorController(MockPFRepository())
+    response = controller.create(person_infos)
+
+    assert response["data"] ["type"] == "Person"
+    assert response["data"] ["count"] == 1
+    assert response["data"] ["attributes"] == person_infos
+
+def test_create_error():
+    person_infos = {
+        "nome_completo": "Djalma da Malasia",
+        "renda_mensal": 1000,
+        "idade": 18,
+        "celular": 11963683670,
+        "email": "djalma_malasiano@gmail.com",
+        "categoria": "ferro",
+        "saldo": 5000
+    }
+
+    controller = PFCreatorController(MockPFRepository())
+    with pytest.raises(Exception):
+        controller.create(person_infos)
