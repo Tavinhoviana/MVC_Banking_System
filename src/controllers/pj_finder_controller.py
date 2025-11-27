40ae9aae9f619806c21392a9ffdf9a7da6ae37b1
Otávio Viana Marques dos Santos <otavio_vms@hotmail.com>
2025-11-27 11:16:22 -0300

    feat: implementing pj_fider_controller with test

---
+++ src/controllers/pj_finder_controller.py
@@ -0,0 +1,36 @@
+from typing import Dict
+from src.models.sqlite.interfaces.pj_repository_interfaces import PJRepositoryInterface
+from src.models.sqlite.entities.pj import PJTable
+
+class PJFinderController:
+    def __init__(self, pj_repository: PJRepositoryInterface) -> None:
+        self.__pj_repository = pj_repository
+
+    def find(self, person_id: int) -> Dict:
+        person = self.__find_person_in_db(person_id)
+        response = self.__format_response(person)
+        return response
+
+    def __find_person_in_db(self, person_id: int) -> PJTable:
+        person = self.__pj_repository.get_person(person_id)
+        if not person:
+            raise Exception("People not found")
+
+        return person
+
+    def __format_response(self, person: PJTable) -> Dict:
+        return {
+            "data": {
+                "type": "Person",
+                "count": 1,
+                "attributes": {
+                    "nome_completo": person.nome_completo,
+                    "renda_mensal": person.renda_mensal,
+                    "idade": person.idade,
+                    "celular": person.celular,
+                    "email": person.email,
+                    "categoria": person.categoria,
+                    "saldo": person.saldo
+                }
+            }
+        }
---
+++ src/controllers/pj_finder_controller_test.py
@@ -0,0 +1,45 @@
+from .pj_finder_controller import PJFinderController
+
+class MockPerson():
+    def __init__(self, renda_mensal, idade, nome_completo, celular, email, categoria, saldo) -> None:
+        self.renda_mensal = renda_mensal
+        self.nome_completo = nome_completo
+        self.idade = idade
+        self.celular = celular
+        self.email = email
+        self.categoria = categoria
+        self.saldo = saldo
+
+class MockPJRepository:
+    def get_person(self, person_id: int):
+        return MockPerson(
+            renda_mensal= 1000,
+            idade= 20,
+            nome_completo= "Guinga Rico",
+            celular= 1196969699696,
+            email= "guinga_rico@gmail.com",
+            categoria= "platinum",
+            saldo= 5000
+        )
+        
+def test_find():
+    controller = PJFinderController(MockPJRepository())
+    response = controller.find(123)
+
+    excepted_response = {
+        "data": {
+                "type": "Person",
+                "count": 1,
+                "attributes": {
+                    "renda_mensal": 1000,
+                    "idade": 20,
+                    "nome_completo": "Guinga Rico",
+                    "celular": 1196969699696,
+                    "email": "guinga_rico@gmail.com",
+                    "categoria": "platinum",
+                    "saldo": 5000
+                }
+            }
+    }
+
+    assert response == excepted_response
